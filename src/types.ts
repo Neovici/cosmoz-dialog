bd12ecdb686a8da4c46322df80d44a70a4930f9c
Husoschi Ionut-Catalin <24230102+ionut@users.noreply.github.com>
2026-06-25 11:33:09 +0300

    feat: update to latest cosmoz-tokens (#14)
    * feat: update to latest cosmoz-tokens
    
    * fix: remove mocha types
    
    * chore: revert to initial dev deps
    
    * fix: rename settings class to icon and correct tsconfig paths alias

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,10 @@
+import { TemplateResult } from 'lit-html';
+
 export interface Props {
+	heading?: string;
+	title?: string;
+	subtitle?: string;
+	icon?: TemplateResult;
 	onClose?: () => void;
 	unmovable?: boolean;
 	closeable?: boolean;
--- stories/cosmoz-dialog.stories.js
+++ stories/cosmoz-dialog.stories.js
@@ -1,14 +1,17 @@
+import { building05Icon, trash01Icon } from '@neovici/cosmoz-icons/untitled';
 import { html } from 'lit-html';
 import { dialog } from '../src/index';
-
+import '../src/loading';
 customElements.define(
 	'demo-dialog',
 	dialog(() => html`<p>Dialog content goes here</p>`),
 );
 
-const Dialog = ({ heading, closeable, unmovable }) => html`
+const Dialog = ({ heading, subtitle, icon, closeable, unmovable }) => html`
 	<demo-dialog
 		.heading=${heading}
+		.subtitle=${subtitle}
+		.icon=${icon}
 		?closeable=${closeable}
 		?unmovable=${unmovable}
 	></demo-dialog>
@@ -16,11 +19,21 @@ const Dialog = ({ heading, closeable, unmovable }) => html`
 
 export default {
 	title: 'Dialog',
+	subtitle: 'A simple dialog component',
 	render: Dialog,
 	argTypes: {
 		heading: {
 			control: 'text',
 			description: 'The title displayed at the top of the dialog',
+		},
+		subtitle: {
+			control: 'text',
+			description: 'The subtitle displayed below the title',
+		},
+
+		icon: {
+			control: false,
+			description: 'A TemplateResult to render as the dialog icon',
 		},
 		closeable: {
 			control: 'boolean',
@@ -35,8 +48,38 @@ export default {
 
 export const Basic = {
 	args: {
-		heading: 'Dialog Title',
+		heading: 'Blog post published',
+		subtitle:
+			'This blog post has been published. Team members will be able to edit this post and republish changes.',
+		icon: null,
 		closeable: true,
 		unmovable: false,
 	},
+};
+
+export const WithIcon = {
+	args: {
+		heading: 'Add your company',
+		subtitle: 'Create your company profile for free in less than 5 minutes.',
+		icon: building05Icon(),
+		closeable: true,
+		unmovable: false,
+	},
+};
+export const WithCustomIcon = {
+	args: {
+		heading: 'Add your company',
+		subtitle: 'Create your company profile for free in less than 5 minutes.',
+		icon: trash01Icon({ styles: 'color:var(--cz-color-text-error)' }),
+		closeable: true,
+		unmovable: false,
+	},
+};
+
+export const Loading = {
+	render: () => html`
+		<cosmoz-dialog-loading heading="Loading" subtitle="Please wait..."
+			>Please wait while we process your request.</cosmoz-dialog-loading
+		>
+	`,
 };
